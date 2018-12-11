49841e65bcac925f819dac439b7a21c08a10779c
Chris Bruno <Chris234k@gmail.com>
2018-12-11 09:56:52 -0500

    Bug fix for the version variable, has to do with how batch file handle variables, i.e 'setlocal enabledelayedexpansion'

--- unity.bat
+++ unity.bat
@@ -8,6 +8,9 @@ REM unity_base_path/[Version Number]/Editor/unity.exe
 REM ---- OR ----
 REM unity_base_path/[Version Number]/Editor/Unity/unity.exe
 
+REM NOTE(chris) necessary for using the 'version' variable to be set during the IF statement
+setlocal enabledelayedexpansion
+
 set unity_base_path=C:\Program Files\Unity Versions
 
 IF "%1"=="" (
@@ -32,23 +35,21 @@ IF "%1"=="" (
     REM 'm_EditorVersion: ' is 17 characters, so we substring to get the actual version number
     REM 'version' is the variable used when calling :DETECT_FOLDER so we set it and proceed as we would if the user typed:
     REM 'unity 2018.2.6f1'
-    set version=%text:~17%
-    echo Detected Version: %version%
+    set version=!text:~17!
+    echo Detected Version: !version!
     
-    REM TODO TODO TODO(chris) at this point we may as well pass an argument to unity.exe to launch the folder as well!
-    
+    REM TODO TODO TODO(chris) at this point we may as well pass an argument to unity.exe to launch the folder as well
     GOTO DETECT_FOLDER
 ) ELSE ( REM an argument has been passed in, and it's not one of the other commands; so we assume it is a version number -- i.e. 'unity 2018.2.6f1'
     set version=%1
-    
     GOTO DETECT_FOLDER
 )
 
 :DETECT_FOLDER
-set unity_path="%unity_base_path%\%version%\Editor\Unity.exe"
+set unity_path="%unity_base_path%\!version!\Editor\Unity.exe"
 
 IF EXIST %unity_path% GOTO LAUNCH_UNITY
-set unity_path="%unity_base_path%\%version%\Unity\Editor\Unity.exe"
+set unity_path="%unity_base_path%\!version!\Unity\Editor\Unity.exe"
 
 :LAUNCH_UNITY
 echo %unity_path%
